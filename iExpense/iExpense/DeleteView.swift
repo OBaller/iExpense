5f1e4ecc4dbb60b5f5515126ff4f087eac91de82
brightsummit.co <naseemoyebola@gmail.com>
2022-01-28 13:28:27 +0100

    Feat: Delete row

---
+++ iExpense/iExpense/DeleteView.swift
@@ -0,0 +1,44 @@
+//
+//  DeleteView.swift
+//  iExpense
+//
+//  Created by naseem on 28/01/2022.
+//
+
+import SwiftUI
+
+struct DeleteView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+struct DeleteView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeleteView()
+    }
+}
